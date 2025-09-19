8f53e4627f2030f856b512c32f0600edd34624df
Fatima Zahra Sabbar <113637835+website-devloper@users.noreply.github.com>
2025-09-19 16:24:43 +0100

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -19,3 +19,21 @@ JOIN user u ON b.user_id = u.user_id
 JOIN property p ON b.property_id = p.property_id
 LEFT JOIN payment pay ON b.booking_id = pay.booking_id;
 
+
+
+
+
+EXPLAIN ANALYZE
+SELECT 
+    b.booking_id,
+    b.check_in,
+    b.check_out,
+    u.user_id,
+    u.name AS user_name,
+    p.property_id,
+    p.name AS property_name,
+    pay.payment_id
+FROM booking b
+JOIN user u ON b.user_id = u.user_id
+JOIN property p ON b.property_id = p.property_id
+LEFT JOIN payment pay ON b.booking_id = pay.booking_id;
