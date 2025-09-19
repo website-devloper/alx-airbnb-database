7f5879f9f34fa302cc95d914e9057bf49fc9a664
Fatima Zahra Sabbar <113637835+website-devloper@users.noreply.github.com>
2025-09-19 16:16:36 +0100

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,1 +1,12 @@
 
+CREATE INDEX idx_user_email ON user(email);
+
+-- Booking table
+CREATE INDEX idx_booking_user ON booking(user_id);
+CREATE INDEX idx_booking_property ON booking(property_id);
+CREATE INDEX idx_booking_check_in ON booking(check_in);
+CREATE INDEX idx_booking_check_out ON booking(check_out);
+
+-- Property table
+CREATE INDEX idx_property_location ON property(location);
+CREATE INDEX idx_property_price ON property(price_per_night);
