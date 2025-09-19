7f201ea966ae690789948183c8da664530e73ba5
Fatima Zahra Sabbar <113637835+website-devloper@users.noreply.github.com>
2025-09-19 16:22:02 +0100

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,4 +1,13 @@
 
+
+EXPLAIN ANALYZE
+SELECT * 
+FROM booking b
+JOIN user u ON b.user_id = u.user_id
+WHERE b.check_in >= '2025-10-01';
+
+
+
 CREATE INDEX idx_user_email ON user(email);
 
 -- Booking table
@@ -10,3 +19,11 @@ CREATE INDEX idx_booking_check_out ON booking(check_out);
 -- Property table
 CREATE INDEX idx_property_location ON property(location);
 CREATE INDEX idx_property_price ON property(price_per_night);
+
+
+EXPLAIN ANALYZE
+SELECT * 
+FROM booking b
+JOIN user u ON b.user_id = u.user_id
+WHERE b.check_in >= '2025-10-01';
+
