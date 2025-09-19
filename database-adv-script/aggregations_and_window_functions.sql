f5c71156d312982a1ec4f6bff84a5e5c487b564e
Fatima Zahra Sabbar <113637835+website-devloper@users.noreply.github.com>
2025-09-19 16:09:34 +0100

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -4,6 +4,12 @@ FROM user u
 LEFT JOIN booking b ON u.user_id = b.user_id
 GROUP BY u.user_id, u.name;
 
+
+
+
+
+
+
 SELECT 
     p.property_id,
     p.name,
@@ -13,6 +19,7 @@ FROM property p
 LEFT JOIN booking b ON p.property_id = b.property_id
 GROUP BY p.property_id, p.name
 ORDER BY property_rank;
+
 
 
 
