f99e298f37ca83937435adb95c5bf8ceef93787d
Fatima Zahra Sabbar <113637835+website-devloper@users.noreply.github.com>
2025-09-19 16:04:30 +0100

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,18 @@
+SELECT name
+FROM property
+WHERE property_id IN (
+    SELECT property_id
+    FROM review
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
 
+
+
+SELECT u.user_id, u.name
+FROM user u
+WHERE (
+    SELECT COUNT(*)
+    FROM booking b
+    WHERE b.user_id = u.user_id
+) > 3;
