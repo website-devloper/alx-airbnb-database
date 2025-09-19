55d00226312ae58a74720a642c73dad36047950f
Fatima Zahra Sabbar <113637835+website-devloper@users.noreply.github.com>
2025-09-19 16:10:34 +0100

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -21,6 +21,15 @@ GROUP BY p.property_id, p.name
 ORDER BY property_rank;
 
 
+SELECT 
+    p.property_id,
+    p.name,
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property_rank
+FROM property p
+LEFT JOIN booking b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name
+ORDER BY property_rank;
 
 
 
